5283cd88fa6997c0ce7aa006ab987d5a1bce2fb7
Adam Duckett <adammichaelduckett@gmail.com>
2016-06-04 09:41:10 +0100

    Livereload .js changes

--- app/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HelloWorld from './helloworld';
-
-ReactDOM.render(
-  <HelloWorld text="Hello World!" />,
-  document.getElementById('app')
-);
--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,11 @@
 var gulp = require('gulp');
 var notify = require('gulp-notify');
 var util = require('gulp-util');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var del = require('del');
 
 // Local Server
 var webserver = require('gulp-webserver');
@@ -23,6 +28,12 @@ var reactDOM = require('react-dom');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+
+// CSS
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-cssnano');
 
 
 /**
@@ -30,20 +41,29 @@ var source = require('vinyl-source-stream');
  */
 var bases = {
   base: './',
-  app: 'app/',
-  web: 'web/'
+  input: 'src/',
+  output: 'dist/'
 };
 
 var paths = {
+  html: {
+    input: bases.input,
+    output: bases.output
+  },
   scripts: {
-    input : bases.app + '**/*.js',
-    output: bases.web + 'js/'
+    input: bases.input + 'js/**/*.js',
+    output: bases.output + 'js/',
+    entry: bases.input + 'js/index.js'
+  },
+  styles: {
+    input: 'src/css/**/*.{scss,sass}',
+    output: 'dist/css/'
   },
   server: {
     host: 'localhost',
-    port: '1337'
-  },
-  html: 'index.html'
+    port: '1337',
+    root: bases.output
+  }
 };
 
 
@@ -51,15 +71,22 @@ var paths = {
  * Gulp Tasks
  */
 
+// Error Handling
+var onError = function ( err ) {
+  util.beep();
+  console.log( err );
+  this.emit( 'end' );
+};
+
 // Start a webserver
-gulp.task( 'webserver', function() {
-  gulp.src( bases.base )
+gulp.task( 'webserver', [ 'copy' ], function() {
+  gulp.src( paths.server.root )
     .pipe(webserver({
       host:             paths.server.host,
       port:             paths.server.port,
       livereload:       true,
       directoryListing: false,
-      fallback: 'index.html'
+      fallback: 'dist/index.html'
     }));
 });
 
@@ -68,27 +95,75 @@ gulp.task('open', function() {
   opn( 'http://' + paths.server.host + ':' + paths.server.port );
 });
 
-// Scripts
+// HTML
+gulp.task('copy', function () {
+  return gulp.src( paths.html.input + 'index.html' )
+  .pipe(gulp.dest( paths.html.output ));
+});
+
+// JS
 gulp.task('scripts', function () {
-  browserify({
-    entries: bases.app + 'app.js',
-    extensions: ['.js'],
+  var bundleStream = browserify({
+    entries: paths.scripts.entry,
+    extensions: [ '.js' ],
     debug: true
   })
-  .transform('babelify')
+  .transform( 'babelify' )
   .bundle()
-  .on('error', util.log)
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest(paths.scripts.output))
-  .pipe(notify({ message: 'Scripts task complete' }))
+
+  return bundleStream
+    .on( 'error', util.log )
+    .pipe(source( 'scripts.js' ))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest( paths.scripts.output ))
+    .pipe(notify({ message: 'Scripts task complete' }))
+});
+
+// CSS
+gulp.task('styles', function() {
+  return gulp.src( paths.styles.input )
+  .pipe(plumber({
+    errorHandler: onError
+  }))
+  .pipe(sass({
+    outputStyle: 'expanded',
+    sourceComments: true
+  }))
+  .pipe(prefix({
+    browsers: [ 'last 2 version' ]
+  }))
+  .pipe(gulp.dest( paths.styles.output ))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(minify({ compatibility: 'ie9' }))
+  .pipe(gulp.dest( paths.styles.output ))
+  .pipe(notify({ message: 'Styles task complete' }))
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del.sync('dist');
 });
 
 
 /**
  * Task Runners
  */
-gulp.task('watch', function(){
-  gulp.watch(paths.scripts.input, ['scripts']);
+gulp.task('watch', [ 'styles', 'scripts', 'copy' ], function() {
+  gulp.watch( 'src/js/**/*.{js,jsx}', [ 'scripts' ] );
+  gulp.watch( 'src/css/**/*.{scss,sass}', [ 'styles' ] );
+  gulp.watch( paths.html.input + 'index.html', [ 'copy' ]);
 });
 
-gulp.task('default', ['scripts', 'webserver', 'watch', 'open']);
+gulp.task('build', function(callback) {
+  runSequence('clean',
+    [ 'scripts', 'styles', 'copy' ],
+    callback
+  )
+});
+
+gulp.task('default', function(callback) {
+  runSequence('build',
+  [ 'webserver', 'watch', 'open' ],
+    callback
+  )
+});
